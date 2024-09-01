60da53ab9dd3b977b882b3c15a144b6b6eb41886
1josee <trnnhattien@gmail.com>
2024-09-01 21:33:45 +0700

    user context + register

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "@/components/Header/Header";
 import { login } from "@/api/auth.api";
 import { ILoginProps } from "@/types/user";
 import axios from "axios";
+import { useUserContext } from "@/app/context";
 
 export default function Login() {
+  const { user } = useUserContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -30,28 +32,17 @@ export default function Login() {
         username: loginData.email,
         password: loginData.password,
       });
-      console.log(response.data.access_token);
-
+      localStorage.setItem("username", response.data.username);
+      localStorage.setItem("role", response.data.role);
+      localStorage.setItem("email", response.data.email);
+      localStorage.setItem("id", response.data.id);
+      localStorage.setItem("accessToken", response.data.access_token);
+      console.log(response.data);
       if (response.data.access_token) {
         window.location.href = "/dashboard";
       } else {
         setError(response.data?.message || "Invalid email or password.");
       }
-      // const response = await fetch('/auth/login', {
-      //     method: 'POST',
-      //     headers: {
-      //         'Content-Type': 'application/json',
-      //     },
-      //     body: JSON.stringify(loginData),
-      // });
-
-      //   if (response.ok) {
-      //     // Redirect to the dashboard or home page
-      //     window.location.href = "/dashboard";
-      //   } else {
-      //     const result = await response.json();
-      //     setError(result.message || "Invalid email or password.");
-      //   }
     } catch (error) {
       const statusCode: string | undefined = error?.toString().split("AxiosError: Request failed with status code ")[1];
       console.log(statusCode);
@@ -67,8 +58,6 @@ export default function Login() {
 
   const [showPassword, setShowPassword] = useState(false);
   const handleShowPassword = () => setShowPassword(!showPassword);
-
-
 
   return (
     <div className="login min-h-screen flex flex-col">
@@ -100,7 +89,7 @@ export default function Login() {
               </label>
               <div className="relative">
                 <input
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                   id="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -114,13 +103,37 @@ export default function Login() {
                 >
                   {showPassword ? (
                     <svg width="20" height="20" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M19.48 19.48C17.6152 20.9015 15.3445 21.6889 13 21.7273C5.36364 21.7273 1 13 1 13C2.35697 10.4712 4.23906 8.26176 6.52 6.52001M10.7091 4.53455C11.46 4.35878 12.2288 4.27092 13 4.27273C20.6364 4.27273 25 13 25 13C24.3378 14.2388 23.5481 15.4052 22.6436 16.48M15.3127 15.3127C15.0131 15.6343 14.6518 15.8922 14.2503 16.071C13.8489 16.2499 13.4155 16.3461 12.9761 16.3539C12.5367 16.3616 12.1002 16.2808 11.6927 16.1162C11.2852 15.9516 10.915 15.7066 10.6042 15.3958C10.2934 15.085 10.0484 14.7149 9.88383 14.3073C9.71923 13.8998 9.63839 13.4633 9.64615 13.0239C9.6539 12.5845 9.75008 12.1511 9.92896 11.7497C10.1078 11.3482 10.3657 10.9869 10.6873 10.6873" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path d="M1 1L25 25" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      <path
+                        d="M19.48 19.48C17.6152 20.9015 15.3445 21.6889 13 21.7273C5.36364 21.7273 1 13 1 13C2.35697 10.4712 4.23906 8.26176 6.52 6.52001M10.7091 4.53455C11.46 4.35878 12.2288 4.27092 13 4.27273C20.6364 4.27273 25 13 25 13C24.3378 14.2388 23.5481 15.4052 22.6436 16.48M15.3127 15.3127C15.0131 15.6343 14.6518 15.8922 14.2503 16.071C13.8489 16.2499 13.4155 16.3461 12.9761 16.3539C12.5367 16.3616 12.1002 16.2808 11.6927 16.1162C11.2852 15.9516 10.915 15.7066 10.6042 15.3958C10.2934 15.085 10.0484 14.7149 9.88383 14.3073C9.71923 13.8998 9.63839 13.4633 9.64615 13.0239C9.6539 12.5845 9.75008 12.1511 9.92896 11.7497C10.1078 11.3482 10.3657 10.9869 10.6873 10.6873"
+                        stroke="black"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M1 1L25 25"
+                        stroke="black"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
                     </svg>
                   ) : (
                     <svg width="20" height="20" viewBox="0 0 26 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1 9.72727C1 9.72727 5.36364 1 13 1C20.6364 1 25 9.72727 25 9.72727C25 9.72727 20.6364 18.4545 13 18.4545C5.36364 18.4545 1 9.72727 1 9.72727Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                      <path d="M12.9998 13C14.8073 13 16.2725 11.5348 16.2725 9.72732C16.2725 7.91984 14.8073 6.45459 12.9998 6.45459C11.1923 6.45459 9.72705 7.91984 9.72705 9.72732C9.72705 11.5348 11.1923 13 12.9998 13Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      <path
+                        d="M1 9.72727C1 9.72727 5.36364 1 13 1C20.6364 1 25 9.72727 25 9.72727C25 9.72727 20.6364 18.4545 13 18.4545C5.36364 18.4545 1 9.72727 1 9.72727Z"
+                        stroke="black"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M12.9998 13C14.8073 13 16.2725 11.5348 16.2725 9.72732C16.2725 7.91984 14.8073 6.45459 12.9998 6.45459C11.1923 6.45459 9.72705 7.91984 9.72705 9.72732C9.72705 11.5348 11.1923 13 12.9998 13Z"
+                        stroke="black"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
                     </svg>
                   )}
                 </div>
---
+++ src/app/context/index.tsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { createContext, useState, useEffect, useContext } from "react";
+
+type User = {
+  username: string;
+  role: string;
+  id: string;
+  email: string;
+};
+
+// Create a UserContext
+const UserContext = createContext<any>(undefined);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User>({ username: "", role: "", id: "", email: "" });
+
+  const handleGetUserData = async (accessToken: string) => {
+    try {
+      // Make the GET request with the access token in the headers
+      const response = await axios.get("http://localhost:8080/auth/profile", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Store the user data in localStorage
+      localStorage.setItem("username", response.data.username);
+      localStorage.setItem("role", response.data.role);
+      localStorage.setItem("email", response.data.email);
+      localStorage.setItem("id", response.data.id);
+
+      // Update the user state
+      setUser({
+        username: response.data.username,
+        role: response.data.role,
+        email: response.data.email,
+        id: response.data.id,
+      });
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      handleGetUserData(accessToken);
+    } else {
+      const storedUsername = localStorage.getItem("username");
+      const storedRole = localStorage.getItem("role");
+      const storedEmail = localStorage.getItem("email");
+      const storedId = localStorage.getItem("id");
+
+      setUser({
+        username: storedUsername || "",
+        role: storedRole || "",
+        email: storedEmail || "",
+        id: storedId || "",
+      });
+    }
+  }, []);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export function useUserContext(){
+    return useContext(UserContext)
+}
