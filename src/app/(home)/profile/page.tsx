c1cc3c53222eb922a6f66cf6b87c0fe27b5bdcbf
ngckhanh <ngockhanh7643@gmail.com>
2024-09-01 23:07:02 +0700

    update

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -1,28 +1,36 @@
 export default function Profile() {
     return (
-        <main className="profile min-h-screen bg-[#BFD2F8] bg-opacity-50 py-8">
+        <main className="profile min-h-screen bg-[#E6F0FF] py-8">
             <div className="container mx-auto px-4 lg:px-8 flex flex-col lg:flex-row gap-8">
+                
                 {/* Personal Profile Section */}
-                <div className="first-container bg-red-300 p-6 rounded-lg shadow-lg flex-1">
-                    <h1 className="text-3xl font-bold mb-4 text-gray-800">Patient Personal Profile</h1>
-                    <p className="text-gray-700">Hi</p>
+                <div className="lg:w-1/3">
+                    <h2 className="text-3xl font-semibold mb-4 text-gray-900">Patient Personal Profile</h2>
+                    <div className="bg-white p-6 rounded-lg shadow-lg">
+
+                        <p className="text-gray-700 text-lg">Hi</p>
+                    </div>
                 </div>
 
-                {/* History Sections */}
-                <div className="second-container flex-1 flex flex-col gap-3">
-                    <h2 className="text-2xl font-semibold mb-2 text-gray-800">Treatment History</h2>
-                    <div className="bg-orange-200 p-6 mb-3 rounded-lg shadow-lg flex flex-col">
-                        <p className="text-gray-700">Hello</p>
+                {/* Treatment and Appointment Sections */}
+                <div className="lg:w-2/3 flex flex-col gap-6">
+                    
+                    <div>
+                        <h2 className="text-3xl font-semibold mb-2 text-gray-900">Treatment</h2>
+                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                            <p className="text-gray-700 text-lg">Hello</p>
+                        </div>
                     </div>
 
-                    <h2 className="text-2xl font-semibold mb-2 text-gray-800">Appointment History</h2>
-                    <div className="bg-pink-200 p-6 rounded-lg shadow-lg flex flex-col">
-                        <p className="text-gray-700">Hello</p>
+                    <div>
+                        <h2 className="text-3xl font-semibold mb-2 text-gray-900">Appointment</h2>
+                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                            <p className="text-gray-700 text-lg">Hello</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </main>
     );
 }
-
-
