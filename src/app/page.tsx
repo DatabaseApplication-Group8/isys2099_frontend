27933eec5fee7f44236b2889a474a0215f784a7a
ngckhanh <ngockhanh7643@gmail.com>
2024-09-04 14:32:28 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function App() {
       <Header />
       <main className="flex flex-grow bg-[#BFD2F8] bg-opacity-[30%] p-8 justify-center items-center">
         <div className='container bg-white p-20 rounded-md max-w-[830px]'>
-          <h1 className="text-3xl font-bold text-center mb-8">Welcome to Hospital Management System!</h1>
-          <div className="text-lg flex flex-col space-y-3">
+          <h1 className="text-3xl font-bold text-center mb-8 text-black">Welcome to Hospital Management System!</h1>
+          <div className="text-lg flex flex-col space-y-3 text-black">
             <p>Lecturer: Mr. Tri Dang Tran</p>
             <p>Team Members: <br/>
             Nguyen Anh Duy (s3878141)<br />
